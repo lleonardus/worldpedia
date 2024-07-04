80b1a79a09153661da7a0c50518bedad9c8b4b2b
lleonardus <leonardus.wizard@gmail.com>
2024-07-04 17:45:30 -0300

    configuring eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
     "react/prop-types": "off",
     "react/no-unescaped-entities": "off",
     "react/jsx-no-target-blank": "off",
+    "no-unused-vars": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
