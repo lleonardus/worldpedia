08caf7e5cfa2d5daf4001929778f9c6e545d9afe
lleonardus <leonardus.wizard@gmail.com>
2024-07-05 12:12:03 -0300

    configuring colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      text: "hsl(var(--color-text) / <alpha-value>)",
+      input: "hsl(var(--color-input) / <alpha-value>)",
+      background: "hsl(var(--color-background) / <alpha-value>)",
+      elements: "hsl(var(--color-elements) / <alpha-value>)",
+      white: "#FFF",
+      black: "#000"
+    },
+    extend: {
+      fontFamily: {
+        sans: "Nunito Sans, sans-serif"
+      }
+    },
   },
   plugins: [],
 };
