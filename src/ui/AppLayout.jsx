5ac02635dd3398febd6529c4ea6b83bf9a51b390
lleonardus <leonardus.wizard@gmail.com>
2024-07-12 14:19:29 -0300

    fixing bug in height

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 
 export function AppLayout() {
   return (
-    <div className="h-screen w-screen bg-background text-text">
+    <div className="h-full min-h-screen w-screen bg-background text-text">
       <Header />
       <Outlet />
     </div>
