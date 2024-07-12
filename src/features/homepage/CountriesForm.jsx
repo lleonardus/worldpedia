94ebeaec225fd43d8f1c1db33be8d5f454227166
lleonardus <leonardus.wizard@gmail.com>
2024-07-12 17:21:24 -0300

    using URL search param (q) to filter countries

--- src/features/homepage/CountriesForm.jsx
+++ src/features/homepage/CountriesForm.jsx
@@ -1,13 +1,35 @@
-import { Form } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Form, useLoaderData, useSubmit } from "react-router-dom";
 import { RegionSelector } from "./RegionSelector";
 
 export function CountriesForm() {
+  const submit = useSubmit();
+  const { q } = useLoaderData();
+  const [query, setQuery] = useState(q || "");
+
+  useEffect(
+    function() {
+      if (q !== undefined) {
+        setQuery(q);
+      }
+    },
+    [q],
+  );
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+    submit(e.currentTarget.form);
+  }
+
   return (
     <div className="flex flex-col gap-10 sm:flex-row sm:justify-between">
       <Form className="relative flex max-w-[35rem] grow">
         <input
-          type="text"
+          type="search"
           placeholder="Search for a country..."
+          name="q"
+          value={query}
+          onChange={(e) => handleChange(e)}
           className="w-full overflow-ellipsis rounded-md bg-elements py-3 pl-20 pr-10 font-semibold text-input shadow-sm focus:outline-none"
         />
         <span className="absolute left-10 top-[55%] translate-y-[-50%]">
--- src/features/homepage/Homepage.jsx
+++ src/features/homepage/Homepage.jsx
@@ -2,9 +2,15 @@ import { CountriesForm } from "./CountriesForm";
 import { CountriesList } from "./CountriesList";
 import { getCountries } from "../../services/apiRestCountries";
 
-export async function loader({ params }) {
-  const countries = await getCountries(params.region);
-  return { countries };
+export async function loader({ params, request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q") || "";
+  let countries = await getCountries(params.region);
+  countries = countries.filter((country) =>
+    country.name.common.toLowerCase().match(q?.toLowerCase() || ""),
+  );
+
+  return { countries, q };
 }
 
 export function Homepage() {
