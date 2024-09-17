4fd0bf081624cf78ea5c7bad22a5ba1750b349ea
lleonardus <leonardus.wizard@gmail.com>
2024-09-17 13:38:55 -0300

    creating Loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Header } from "./Header";
+import { Loader } from "./Loader";
 
 export function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="h-full min-h-screen w-screen bg-background pb-20 text-text">
+      {isLoading && <Loader />}
       <Header />
       <Outlet />
     </div>
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,7 @@
+export function Loader() {
+  return (
+    <div className="fixed inset-0 z-[9999] flex justify-center bg-black/60 pt-[300px]">
+      <div className="loader"></div>
+    </div>
+  );
+}
